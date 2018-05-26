e81c9185c93e1a7ac27e52e788ec9c26da16d909
davidliu <david-khala@hotmail.com>
2018-05-26 15:22:34 +0800

    hash as an invoker

--- express/hashFile.js
+++ express/hashFile.js
@@ -3,15 +3,13 @@ const router = express.Router();
 const logger = require('../app/util/logger').new('hash record');
 const Request = require('request');
 
-const Multer = require('multer');
-const cache = Multer({dest: 'cache/'});
 
 const fs = require('fs');
-const hashAlgo = require('fabric-client/lib/hash').sha2_256;
 
 const helper = require('../app/helper.js');
 const invalid = require('./formValid').invalid();
-const {reducer} = require('../app/util/chaincode');
+const { reducer } = require('../app/util/chaincode');
+const { invoke } = require('../app/invoke-chaincode.js');
 
 const channelName = 'allChannel';
 const chaincodeId = 'hashChaincode';
@@ -19,7 +17,9 @@ const TKOrgName = 'TK.Teeking.com'
 const globalConfig = require('../config/orgs.json');
 const TKMSP = globalConfig.orgs['TK.Teeking.com'].MSP.id
 
-const baseUrl = 'http://192.168.3.97:8080/teeking-api/api';
+const successHandle = (data, res) => {
+	res.json({ result: true, data });
+}
 const errorHandle = (err, res) => {
 	const errorCodeMap = require('./errorCodeMap.json');
 
@@ -30,279 +30,145 @@ const errorHandle = (err, res) => {
 			break;
 		}
 	}
-	res.status(status).json({error: err.toString()});
+	res.status(status).json({ result: false, error: err.toString() });
 
 };
-
-
-const oauthMiddleware = (req, res, next) => {
-	const {username, password, token} = req.body;
-	const oauthClient = require('./oauth2Client').passwordGrant;
-	let promise = Promise.resolve();
-
-	const oauthEnable = false;
-	if (oauthEnable) {
-		if (!token) {
-			if (!username || !password) {
-				return errorHandle('missing user identity ', res);
-			}
-			promise = promise.then(() => oauthClient.getToken({username, password}))
-				.then(accessToken => {
-					res.locals.accessToken = accessToken.accessToken.accessToken;
-					next();
-					return Promise.resolve(accessToken);
-				});
-		} else {
-			//TODO request TK platform to authenticate token
-			promise = promise.then(() => oauthClient.verify(token)).then(result => {
-				if (result) {
-					res.locals.accessToken = token;
-					next();
-					return Promise.resolve(result);
-				}
-			});
-		}
-	} else {
-		promise = promise.then(() => new Promise((resolve, reject) => {
-			Request.post({
-				url: `${baseUrl}/tokens`, form: {
-					name: username, password
-				}
-			}, (err, resp, body) => {
-				if (err) return reject(err);
-				logger.debug('tokens','response',body)
-				const {data:{token}, userid} = JSON.parse(body);
-				res.locals.accessToken = token;
-				next();
-				return resolve();
-			});
-		}));
-
-	}
-
-	promise.catch(err => {
-		res.status(400).send(err);
-	});
-};
+const paramChecker = ({ key, orgName, args }) => {
+	const invalidChannelName = invalid.channelName({ channelName });
+	if (invalidChannelName) throw invalidChannelName;
+	if (!key) throw `id is ${key}`;
+	if (!orgName) throw `org is ${orgName}`;
+	const invalidPeer = invalid.peer({ peerIndex, orgName });
+	if (invalidPeer) throw invalidPeer;
+	const invalidArgs = invalid.args({ args });
+	if (invalidArgs) throw invalidArgs;
+}
 
 const peerIndex = 0;
 
-router.post('/write', cache.array('files'), (req, res) => {
-	const {id, plain, toHash} = req.body;
-	let {accessToken} = res.locals;
-	
-	const {org: orgName} = req.body;
-	logger.debug('write', {id, plain, toHash, orgName});
+router.post('/write', async (req, res) => {
+	const { key, hash } = req.body;
 
-	if (!id) return errorHandle(`id is ${id}`, res);
-	if (!orgName) return errorHandle(`org is ${orgName}`, res);
+	const { org: orgName } = req.body;
+	logger.debug('write', { key, hash, orgName });
 
-	const invalidPeer = invalid.peer({peerIndex, orgName});
-	if (invalidPeer) return errorHandle(invalidPeer, res);
-	const files = req.files;
-
-	let promise = Promise.resolve();
-
-	const fileStreams = files.map(({path}) => {
-		return fs.createReadStream(path);
-	});
-
-
-	const uploadFilesRequest = () => new Promise((resolve, reject) => {
-		const formData = {
-			accessToken,
-			files: fileStreams,
-		};
-		Request.post({url: `${baseUrl}/files`, formData,timeout:1000}, (err, resp, body) => {
-			logger.debug({err,body});
-			if (err) {
-				return reject(err);
-			}
-			logger.debug('Upload successful!  Server responded with:', body);
-			resolve(JSON.parse(body))
-		});
-	});
-
-	const assetsRequest = () => new Promise((resolve, reject) => {
-		const form = {
-			token:accessToken, type:plain, data:toHash
-		};
-
-		Request.post({url: `${baseUrl}/blockchains`, form}, (err, resp, body) => {
-			if (err) {
-				return reject(err);
-			}
-			if(!body){
-				return reject(resp)
-			}
-			logger.debug('sql server response with:', body);
-			resolve(JSON.parse(body));
-
-		});
-	});
-	promise = promise
-		// .then(uploadFilesRequest)
-		.then(assetsRequest)
-		.then(body => {
-			const {success, message, data:{ischain, req_data, hash}} = body;
-			const fcn = 'write';
-			const args = [id, hash];
-
-			logger.debug({chaincodeId, fcn, orgName, peerIndex, channelName});
-			const invalidArgs = invalid.args({args});
-			if (invalidArgs) reject(invalidArgs);
-
-			return Promise.resolve({fcn:ischain?fcn:undefined, args});
+	try {
+		const fcn = 'write';
+		const args = [key, hash];
+		paramChecker({ key, orgName, args })
+		logger.debug({ chaincodeId, fcn, orgName, peerIndex, channelName });
+		const client = await helper.getOrgAdmin(orgName)
+		const channel = helper.prepareChannel(channelName, client);
+		const peers = helper.newPeers([peerIndex], orgName);
+		const message = await invoke(channel, peers, {
+			chaincodeId, fcn,
+			args
 		})
-		.then(({fcn, args}) => {
-			if (!fcn) {
-				res.json({data: 'not to update blockchain'});
-				return;
-			}
-			const {invoke} = require('../app/invoke-chaincode.js');
-			return helper.getOrgAdmin(orgName).then((client) => {
-				const channel = helper.prepareChannel(channelName, client);
-				const peers = helper.newPeers([peerIndex], orgName);
-				return invoke(channel, peers, {
-					chaincodeId, fcn,
-					args
-				}).then((message) => {
-					const data = reducer(message);
-					res.json({data: data.responses});
+		const data = reducer(message);
+		successHandle(data.responses, res);
 
-				});
-			});
-		});
-
-	promise.catch(err => {
+	} catch (err) {
 		logger.error(err);
-		const {proposalResponses} = err;
+		const { proposalResponses } = err;
 		if (proposalResponses) {
 			errorHandle(proposalResponses, res);
 		} else {
 			errorHandle(err, res);
 		}
-	});
-
-});
-router.post('/delete', (req, res) => {
-	const {id} = req.body;
-	const {org: orgName} = req.body;
-	logger.debug('delete', {id, orgName});
-
-	if (!id) return errorHandle(`id is ${id}`, res);
-	if (!orgName) return errorHandle(`org is ${orgName}`, res);
-	const fcn = 'delete';
-	const args = [id];
-	const {invoke} = require('../app/invoke-chaincode.js');
-
-	logger.debug({chaincodeId, fcn, orgName, peerIndex, channelName});
-	const invalidPeer = invalid.peer({peerIndex, orgName});
-	if (invalidPeer) return errorHandle(invalidPeer, res);
-
-	const invalidChannelName = invalid.channelName({channelName});
-	if (invalidChannelName) return errorHandle(invalidChannelName, res);
-
-	const invalidArgs = invalid.args({args});
-	if (invalidArgs) return errorHandle(invalidArgs, res);
-
-	helper.getOrgAdmin(orgName).then((client) => {
-		const channel = helper.prepareChannel(channelName, client);
-		const peers = helper.newPeers([peerIndex], orgName);
-		return invoke(channel, peers, {
-			chaincodeId, fcn,
-			args
-		}).then((message) => {
-			const data = reducer(message);
-			res.json({data: data.responses});
-
-		}).catch(err => {
-			logger.error(err);
-			const {proposalResponses} = err;
-			if (proposalResponses) {
-				errorHandle(proposalResponses, res);
-			} else {
-				errorHandle(err, res);
-			}
-		});
-	});
-});
-router.post('/read', (req, res) => {
-	const {id} = req.body;
-	const {org: orgName} = req.body;
-	logger.debug('read', {id, orgName});
-
-	if (!id) return errorHandle(`id is ${id}`, res);
-	if (!orgName) return errorHandle(`org is ${orgName}`, res);
-
-	const fcn = 'read';
-
-	const args = [id];
-	if (orgName === TKOrgName) {
-		args.push(req.body.delegatedMSP?req.body.delegatedMSP:TKMSP);
 	}
-	const {invoke} = require('../app/invoke-chaincode.js');
 
-	logger.debug({chaincodeId, fcn, orgName, peerIndex, channelName});
-	const invalidPeer = invalid.peer({peerIndex, orgName});
-	if (invalidPeer) return errorHandle(invalidPeer, res);
+});
+router.post('/delete', async (req, res) => {
+	const { key, org: orgName } = req.body;
+	logger.debug('delete', { key, orgName });
 
-	const invalidChannelName = invalid.channelName({channelName});
-	if (invalidChannelName) return errorHandle(invalidChannelName, res);
+	try {
+		const fcn = 'delete';
+		const args = [key];
+		paramChecker({ key, orgName, args })
 
-	const invalidArgs = invalid.args({args});
-	if (invalidArgs) return errorHandle(invalidArgs, res);
-
-	helper.getOrgAdmin(orgName).then((client) => {
+		logger.debug({ chaincodeId, fcn, orgName, peerIndex, channelName });
+		const client = await helper.getOrgAdmin(orgName)
 		const channel = helper.prepareChannel(channelName, client);
 		const peers = helper.newPeers([peerIndex], orgName);
-		return invoke(channel, peers, {
+		const message = await invoke(channel, peers, {
 			chaincodeId, fcn,
 			args
-		}).then((message) => {
-			const data = reducer(message);
-			res.json({data: data.responses});
+		})
+		const data = reducer(message);
+		successHandle(data.responses, res);
 
-		}).catch(err => {
-			logger.error(err);
-			const {proposalResponses} = err;
-			if (proposalResponses) {
-				errorHandle(proposalResponses, res);
-			} else {
-				errorHandle(err, res);
-			}
-		});
-	});
+	} catch (err) {
+		logger.error(err);
+		const { proposalResponses } = err;
+		if (proposalResponses) {
+			errorHandle(proposalResponses, res);
+		} else {
+			errorHandle(err, res);
+		}
+	}
+});
+router.post('/read', async (req, res) => {
+	const { key, org: orgName } = req.body;
+	logger.debug('read', { key, orgName });
+
+	try {
+		const fcn = 'read';
+
+		const args = [key];
+		paramChecker({ key, orgName, args })
+		if (orgName === TKOrgName) {
+			args.push(req.body.delegatedMSP ? req.body.delegatedMSP : TKMSP);
+		}
+		logger.debug({ chaincodeId, fcn, orgName, peerIndex, channelName });
+
+		const client = await helper.getOrgAdmin(orgName)
+		const channel = helper.prepareChannel(channelName, client);
+		const peers = helper.newPeers([peerIndex], orgName);
+		const message = await invoke(channel, peers, {
+			chaincodeId, fcn,
+			args
+		})
+		const data = reducer(message);
+		successHandle(data.responses, res);
+	} catch (err) {
+		logger.error(err);
+		const { proposalResponses } = err;
+		if (proposalResponses) {
+			errorHandle(proposalResponses, res);
+		} else {
+			errorHandle(err, res);
+		}
+	}
 });
 
-router.get('/worldState', (req, res) => {
+router.post('/worldState', async (req, res) => {
 
-
+	const {  org: orgName } = req.body;
 	const fcn = 'worldStates';
 
 	const args = [];
-	const {invoke} = require('../app/invoke-chaincode.js');
 
-	helper.getOrgAdmin(TKOrgName).then((client) => {
+	try {
+		const client = await helper.getOrgAdmin(orgName)
 		const channel = helper.prepareChannel(channelName, client);
-		const peers = helper.newPeers([peerIndex], TKOrgName);
-		return invoke(channel, peers, {
+		const peers = helper.newPeers([peerIndex], orgName);
+		const message = await invoke(channel, peers, {
 			chaincodeId, fcn,
 			args
-		}).then((message) => {
-			const data = reducer(message);
-			res.json({data: JSON.parse(data.responses)});
+		})
+		const data = reducer(message);
+		successHandle(JSON.parse(data.responses), res);
+	} catch (err) {
+		logger.error(err);
+		const { proposalResponses } = err;
+		if (proposalResponses) {
+			errorHandle(proposalResponses, res);
+		} else {
+			errorHandle(err, res);
+		}
+	}
 
-		}).catch(err => {
-			logger.error(err);
-			const {proposalResponses} = err;
-			if (proposalResponses) {
-				errorHandle(proposalResponses, res);
-			} else {
-				errorHandle(err, res);
-			}
-		});
-	});
 });
 
 module.exports = router;
